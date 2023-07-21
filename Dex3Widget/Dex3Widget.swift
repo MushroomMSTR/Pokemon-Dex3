dcd9b413f27ad6e2b87705731ba409f0af1715f3
NazarStf <hworker98@gmail.com>
2023-07-21 21:17:56 +0300

    Add comments for Widget code explanation

--- Dex3Widget/Dex3Widget.swift
+++ Dex3Widget/Dex3Widget.swift
@@ -10,7 +10,12 @@ import SwiftUI
 import Intents
 import CoreData
 
+// MARK: - Timeline Provider
+// This struct is a timeline provider for the WidgetKit framework. It generates the timeline for the widget.
 struct Provider: IntentTimelineProvider {
+
+	// MARK: - Random Pokemon Provider
+	// This function provides a random Pokemon from Core Data or a sample Pokemon if Core Data fetch fails
 	var randomPokemon: Pokemon {
 		let context = PersistenceController.shared.container.viewContext
 
@@ -31,43 +36,51 @@ struct Provider: IntentTimelineProvider {
 		return SamplePokemon.samplePokemon
 	}
 	
-    func placeholder(in context: Context) -> SimpleEntry {
+	// MARK: - Placeholder
+	// This function provides a placeholder for the widget when the widget is being prepared to be shown.
+	func placeholder(in context: Context) -> SimpleEntry {
 		SimpleEntry(date: Date(), configuration: ConfigurationIntent(), pokemon: SamplePokemon.samplePokemon)
-    }
+	}
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration, pokemon: randomPokemon)
-        completion(entry)
-    }
+	// MARK: - Snapshot
+	// This function provides a snapshot of the widget for quick updates
+	func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+		let entry = SimpleEntry(date: Date(), configuration: configuration, pokemon: randomPokemon)
+		completion(entry)
+	}
 
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+	// MARK: - Timeline
+	// This function generates a timeline for the widget with multiple entries
+	func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+		var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration, pokemon: randomPokemon)
-            entries.append(entry)
-        }
+		let currentDate = Date()
+		for hourOffset in 0 ..< 5 {
+			let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+			let entry = SimpleEntry(date: entryDate, configuration: configuration, pokemon: randomPokemon)
+			entries.append(entry)
+		}
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
+		let timeline = Timeline(entries: entries, policy: .atEnd)
+		completion(timeline)
+	}
 }
 
+// MARK: - Simple Entry
+// This struct represents a single timeline entry, which includes the date, configuration and a Pokemon.
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationIntent
-	
+	let date: Date
+	let configuration: ConfigurationIntent
 	let pokemon: Pokemon
 }
 
+// MARK: - Dex3 Widget Entry View
+// This struct is the main view for the widget entry. It shows a Pokemon based on the widget size.
 struct Dex3WidgetEntryView : View {
 	@Environment(\.widgetFamily) var widgetSize
-    var entry: Provider.Entry
+	var entry: Provider.Entry
 
-    var body: some View {
+	var body: some View {
 		switch widgetSize {
 		case .systemSmall:
 			WidgetPokemon(widjetSize: .small)
@@ -82,23 +95,27 @@ struct Dex3WidgetEntryView : View {
 			WidgetPokemon(widjetSize: .large)
 				.environmentObject(entry.pokemon)
 		}
-    }
+	}
 }
 
+// MARK: - Dex3 Widget
+// This struct represents the widget itself.
 struct Dex3Widget: Widget {
-    let kind: String = "Dex3Widget"
+	let kind: String = "Dex3Widget"
 
-    var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            Dex3WidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
+	var body: some WidgetConfiguration {
+		IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+			Dex3WidgetEntryView(entry: entry)
+		}
+		.configurationDisplayName("My Widget")
+		.description("This is an example widget.")
+	}
 }
 
+// MARK: - Widget Previews
+// This struct provides previews of the widget in different sizes.
 struct Dex3Widget_Previews: PreviewProvider {
-    static var previews: some View {
+	static var previews: some View {
 		Group {
 			Dex3WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), pokemon: SamplePokemon.samplePokemon))
 				.previewContext(WidgetPreviewContext(family: .systemSmall))
@@ -109,5 +126,5 @@ struct Dex3Widget_Previews: PreviewProvider {
 			Dex3WidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), pokemon: SamplePokemon.samplePokemon))
 				.previewContext(WidgetPreviewContext(family: .systemLarge))
 		}
-    }
+	}
 }
